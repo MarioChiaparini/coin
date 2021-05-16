4fff2df5f27048ca0ad26c7266ac49612145d454
Mario_Chiaparini <45772937+MarioChiaparini@users.noreply.github.com>
2021-05-16 09:36:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.get('/eth.html', (req, res) => {
 //add the router
 app.use('/', router);
 
-app.listen(process.env.Port || 3000);
+app.listen(process.env.PORT || 3000);
 
 console.log('Running at Port 3000');
